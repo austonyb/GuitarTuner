69e6049746846b639ab454ad9a0301fef758a575
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-06-04 17:50:49 -0600

    Initial Commit

---
+++ GuitarTuner/GuitarTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTunerApp.swift
+//  GuitarTuner
+//
+//  Created by Auston Youngblood on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GuitarTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
