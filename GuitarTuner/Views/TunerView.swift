8ff6d2868a89ab52e87569f46b20c0fb70047218
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-06-04 21:25:26 -0600

    changed initial view to TunerView

---
+++ GuitarTuner/Views/TunerView.swift
@@ -0,0 +1,20 @@
+//
+//  InitialView.swift
+//  GuitarTuner
+//
+//  Created by Auston Youngblood on 6/4/23.
+//
+
+import SwiftUI
+
+struct TunerView: View {
+    var body: some View {
+        Text("Tuner Screen")
+    }
+}
+
+struct InitialView_Previews: PreviewProvider {
+    static var previews: some View {
+        TunerView()
+    }
+}
